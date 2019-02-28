35180a491ea01dac4f09f30a43bfd165fd1b462a
vagrant <vagrant@localhost.localdomain>
2019-02-28 23:39:03 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,19 @@
+package 'tree' do
+  package_name 'tree'
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Temitope Ajisafe'
+  group 'root'
+  owner 'root'
+  action :create
+end
+
+package 'git' do
+  action :install
+end
